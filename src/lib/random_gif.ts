3ff40f24ef5b34bbf573cfe73860182a4df9971b
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-02-04 14:17:06 +0530

    feat: more random

--- src/lib/random_gif.ts
+++ src/lib/random_gif.ts
@@ -3,8 +3,15 @@ import gifs from "./gifs.json";
 export function getRandomUrl():  URL{
     // get a random object from @gifs
     const jobj = JSON.parse(JSON.stringify(gifs));
-    const length = jobj.length;
-    const random = Math.floor(Math.random() * length);
+    const length = jobj.length-1;
+    
+    const r1 = Math.floor(Math.random() * length);
+    const r2 = Math.floor(Math.random() * length);
+    let random = r1 + r2;
+    random = random ? random > length? random - length : random : 0;
+    
+    console.log("random: " + random + " length: " + length + " r1: " + r1 + " r2: " + r2);
+    
     const obj = jobj[random].url;
     // get the url from the object
     const url = new URL(obj);
