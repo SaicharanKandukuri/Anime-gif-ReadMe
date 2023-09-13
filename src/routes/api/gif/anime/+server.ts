9cb2f135400319321761a476ebe2f0b2717e7094
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-09-13 13:04:08 +0530

    bochii desu.....

--- src/routes/api/gif/anime/+server.ts
+++ src/routes/api/gif/anime/+server.ts
@@ -1,11 +1,18 @@
 import { json } from "@sveltejs/kit";
-import { getRandomUrl } from "../../../../lib/random_gif";
+import { getRandomUrl } from "$lib/random_gif.ts";
 
 export async function GET({request}: any) {
     // https://user-images.githubusercontent.com/68287637/212882218-4bcc2eae-08a4-4175-a8cc-afb00ed0087a.gif
     const imgurl = getRandomUrl();
     // get the image
-    const img = await fetch(imgurl);
+
+    let img;
+    try {
+        img = await fetch(imgurl);
+    } catch (e) {
+        console.log(e);
+        return new Response("Error fetching image", {status: 500});
+    }
     // get the image as a blob
     const blob = await img.blob();
 
