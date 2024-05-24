695d8278682bbd0f2e630af6688b1bcab886f209
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2024-05-24 10:02:19 +0530

    chore: use array instead for gif urls

--- src/lib/random_gif.ts
+++ src/lib/random_gif.ts
@@ -3,7 +3,9 @@ import gifs from "./gifs.json";
 export function getRandomUrl():  URL{
     // get a random object from @gifs
     const jobj = JSON.parse(JSON.stringify(gifs));
-    const length = jobj.length-1;
+    const length = jobj.gifs.length - 2;
+    console.log("=> ", length)
+    
     
     const r1 = Math.floor(Math.random() * length);
     const r2 = Math.floor(Math.random() * length);
@@ -12,7 +14,7 @@ export function getRandomUrl():  URL{
     
     console.log("random: " + random + " length: " + length + " r1: " + r1 + " r2: " + r2);
     
-    const obj = jobj[random].url;
+    const obj = jobj.gifs[random];
     // get the url from the object
     const url = new URL(obj);
     return url;
